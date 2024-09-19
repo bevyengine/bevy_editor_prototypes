eaea4191a8a47bb5dc2b24002c442e208a40ffd0
Jan Hohenheim <jan@hohenheim.ch>
2024-09-19 20:16:47 +0200

    Add library test to make CI happy (#16)
    * Add library test
    
    * Remove using

--- src/main.rs
+++ src/main.rs
@@ -3,3 +3,11 @@ use bevy::prelude::*;
 fn main() -> AppExit {
     App::new().add_plugins(DefaultPlugins).run()
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert!(true, "it works!");
+    }
+}

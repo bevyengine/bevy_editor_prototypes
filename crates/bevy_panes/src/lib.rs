a9bb0143348f6230ae6084b2b55d914916fec55d
Atlas16A <thegoldenatlaspass@gmail.com>
2024-09-22 18:56:44 -0400

    Reserve components necessary for implementing Pane Layout system.

--- crates/bevy_panes/src/lib.rs
+++ crates/bevy_panes/src/lib.rs
@@ -1,5 +1,5 @@
 //! Resizable, draggable, collapsible and dockable panes for Bevy.
-
+use bevy::prelude::*;
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
@@ -14,3 +14,33 @@ mod tests {
         assert_eq!(result, 4);
     }
 }
+
+#[derive(Component)]
+pub struct RootPaneLayoutNode;
+
+#[derive(Component)]
+pub struct PaneRootNode;
+
+#[derive(Component)]
+pub struct PaneAreaNode;
+
+#[derive(Component)]
+pub struct PaneHeaderNode;
+
+#[derive(Component)]
+pub struct PaneMenuButtonNode;
+
+#[derive(Component)]
+pub struct PaneSplitterNode;
+
+#[derive(Component)]
+pub struct TopResizeBarNode;
+
+#[derive(Component)]
+pub struct BottomResizeBarNode;
+
+#[derive(Component)]
+pub struct LeftResizeBarNode;
+
+#[derive(Component)]
+pub struct RightResizeBarNode;

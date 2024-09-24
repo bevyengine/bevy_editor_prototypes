f2f75495e486550c18306dc21544a14c3717b863
Atlas16A <41647436+Atlas16A@users.noreply.github.com>
2024-09-24 08:12:48 -0400

    Make Bevy dependency repo Main branch & Reserve components for editor layout system (#24)
    * Change bevy Dep to be main branch of bevy
    
    * Reserve components necessary for implementing Pane Layout system.
    
    * Fix previous cargo.toml commit error
    
    * Update Cargo.toml
    
    * Branch and rev are redundant
    
    * fix: bevy dependency specification
    
    * fix: pin bevy commit
    
    * Change deps following BD103 suggestion
    
    * Add simple docs
    
    * refactor: rustfmt
    
    ---------
    
    Co-authored-by: Alice Cecile <alice.i.cecile@gmail.com>
    Co-authored-by: BD103 <59022059+BD103@users.noreply.github.com>

--- crates/bevy_panes/src/lib.rs
+++ crates/bevy_panes/src/lib.rs
@@ -1,6 +1,7 @@
 //! Resizable, draggable, collapsible and dockable panes for Bevy.
+use bevy::prelude::*;
 
-/// an add function that adds two numbers
+/// Basic add function, will be removed later.
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
@@ -15,3 +16,43 @@ mod tests {
         assert_eq!(result, 4);
     }
 }
+
+/// Root node to capture all editor UI elements, nothing but the layout system should modify this.
+#[derive(Component)]
+pub struct RootPaneLayoutNode;
+
+/// Root node for each pane, holds all event nodes for layout and the basic structure for all Panes.
+#[derive(Component)]
+pub struct PaneRootNode;
+
+/// Node to denote the content area of the Pane.
+#[derive(Component)]
+pub struct PaneAreaNode;
+
+/// Node to add widgets into the header of a Pane.
+#[derive(Component)]
+pub struct PaneHeaderNode;
+
+/// Button to open up Pane selection menu.
+#[derive(Component)]
+pub struct PaneMenuButtonNode;
+
+/// Represents the corners and holds an observer to split/merge panes.
+#[derive(Component)]
+pub struct PaneSplitterNode;
+
+/// Resizing bar for top of the pane.
+#[derive(Component)]
+pub struct TopResizeBarNode;
+
+/// Resizing bar for bottom of the pane.
+#[derive(Component)]
+pub struct BottomResizeBarNode;
+
+/// Left hand resize bar.
+#[derive(Component)]
+pub struct LeftResizeBarNode;
+
+/// Right hand resize bar.
+#[derive(Component)]
+pub struct RightResizeBarNode;

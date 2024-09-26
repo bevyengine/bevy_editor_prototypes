98f09368a1291260cc91487a4e678b691ad5e91c
Atlas16A <41647436+Atlas16A@users.noreply.github.com>
2024-09-25 20:46:07 -0400

    Add Docs covering layout system, Rename bevy_panes to bevy_pane_layout (#41)

--- crates/bevy_pane_layout/src/lib.rs
+++ crates/bevy_pane_layout/src/lib.rs
@@ -1,4 +1,17 @@
-//! Resizable, draggable, collapsible and dockable panes for Bevy.
+//! Resizable, split-able panes for Bevy.
+/// The Bevy Pane Layout system.
+/// The intent of this system is to provide a way to create resizable, split-able panes in Bevy.
+/// Mimicking the behavior of of Blender's layout system.
+///
+/// Blender Documentation: <https://docs.blender.org/manual/en/latest/interface/window_system/areas.html>
+///
+/// Requirements for a valid Pane:
+/// - All panes must fit within their bounds, no overflow is allowed.
+/// - Panes can not have power over the layout system, their dimensions are controlled by the layout system and should not be modified by anything else.
+/// - All panes must have a header, a content area, however a footer is optional.
+/// - Panes cannot have min/max sizes, they must be able to be resized to any size.
+///   - If a pane can not be sensibly resized, it can overflow under the other panes.
+/// - Panes must not interfere with each other, only temporary/absolute positioned elements are allowed to overlap panes.
 use bevy::prelude::*;
 
 /// Basic add function, will be removed later.

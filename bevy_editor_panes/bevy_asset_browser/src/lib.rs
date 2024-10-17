6e354f32aacd4dfaf5a5de86346c97a2cbe5fe51
hcabel <coding@hugocabel.com>
2024-10-17 16:09:45 +1000

    Assetbrowser rewrite to use FileAssetReader instead of std::fs

--- bevy_editor_panes/bevy_asset_browser/src/lib.rs
+++ bevy_editor_panes/bevy_asset_browser/src/lib.rs
@@ -1,17 +1,19 @@
 //! A UI element for browsing assets in the Bevy Editor.
 /// The intent of this system is to provide a simple and frictionless way to browse assets in the Bevy Editor.
 /// The asset browser is a replica of the your asset directory on disk and get's automatically updated when the directory is modified.
-use std::{
-    path::{Path, PathBuf},
-    time::SystemTime,
-};
+use std::{path::PathBuf, time::SystemTime};
 
+use atomicow::CowArc;
 use bevy::{
-    asset::{embedded_asset, io::file::FileAssetReader},
+    asset::{
+        embedded_asset,
+        io::{AssetSource, AssetSourceBuilders, AssetSourceId},
+    },
+    ecs::system::SystemId,
     prelude::*,
 };
 use bevy_editor_styles::Theme;
-use directory_content::{DirectoryContentNode, ScrollingList};
+use directory_content::DirectoryContentNode;
 use top_bar::TopBarNode;
 
 mod directory_content;
@@ -23,17 +25,63 @@ pub struct AssetBrowserPlugin;
 impl Plugin for AssetBrowserPlugin {
     fn build(&self, app: &mut App) {
         embedded_asset!(app, "assets/directory_icon.png");
+        embedded_asset!(app, "assets/source_icon.png");
         embedded_asset!(app, "assets/file_icon.png");
         app.insert_resource(AssetBrowserLocation::default())
+            .insert_resource(directory_content::DirectoryContent::default())
+            .init_resource::<AssetBrowserOneShotSystems>()
             .insert_resource(DirectoryLastModifiedTime(SystemTime::UNIX_EPOCH))
-            .add_systems(Startup, ui_setup.in_set(AssetBrowserSet))
+            .add_systems(
+                Startup,
+                (
+                    ui_setup.in_set(AssetBrowserSet),
+                    directory_content::fetch_directory_content,
+                ),
+            )
             .add_systems(
                 Startup,
                 (top_bar::ui_setup, directory_content::ui_setup).after(AssetBrowserSet),
             )
-            .add_systems(Update, button_interaction)
-            .add_systems(Update, directory_content::scrolling)
-            .add_systems(FixedUpdate, refresh_directory_content);
+            .add_systems(
+                Startup,
+                (
+                    top_bar::refresh_location_ui.after(top_bar::ui_setup),
+                    // directory_content::refresh_ui.after(directory_content::ui_setup),
+                ),
+            )
+            .add_systems(Update, (button_interaction, directory_content::scrolling))
+            .add_systems(
+                Update,
+                (
+                    directory_content::poll_fetch_task
+                        .run_if(directory_content::run_if_fetch_task_is_running),
+                    directory_content::refresh_ui
+                        .after(directory_content::poll_fetch_task)
+                        .run_if(directory_content::run_if_content_as_changed),
+                ),
+            );
+    }
+}
+
+/// All the asset browser one shot systems
+pub enum OneShotSystem {
+    /// Refer to the system that fetches the directory content on disk
+    FetchDirectoryContent,
+    /// Refer to the system that refreshes the top bar UI
+    RefreshTopBarUi,
+}
+
+/// All the asset browser one shot systems, see [`OneShotSystem`] enum for reference
+#[derive(Resource)]
+pub struct AssetBrowserOneShotSystems(pub [SystemId; 2]);
+
+impl FromWorld for AssetBrowserOneShotSystems {
+    fn from_world(world: &mut World) -> Self {
+        // Order is important here! Should match the order of OneShotSystem enum
+        Self([
+            world.register_system(directory_content::fetch_directory_content),
+            world.register_system(top_bar::refresh_location_ui),
+        ])
     }
 }
 
@@ -42,50 +90,21 @@ impl Plugin for AssetBrowserPlugin {
 pub struct AssetBrowserSet;
 
 /// The current location of the asset browser
-#[derive(Resource)]
+// TODO: Make this an AssetPath
+#[derive(Resource, Debug, Clone, PartialEq, Eq)]
 pub struct AssetBrowserLocation {
-    absolute_path: PathBuf,
-    start_segment_index: usize,
+    /// The source id of the asset source to browse
+    pub source_id: Option<AssetSourceId<'static>>,
+    /// The path of the current directory relative to the asset source root
+    pub path: PathBuf,
 }
 
 impl Default for AssetBrowserLocation {
     fn default() -> Self {
-        let absolute_path = FileAssetReader::get_base_path().join("assets");
-        let start_segment_index = absolute_path.iter().count() - 1;
         Self {
-            absolute_path,
-            start_segment_index,
+            source_id: Some(AssetSourceId::Default),
+            path: PathBuf::from(""),
         }
-    }
-}
-
-impl AssetBrowserLocation {
-    /// Get the path starting from the `start_segment_index`.
-    pub fn get_path(&self) -> &Path {
-        let mut comps = self.absolute_path.as_path().components();
-        for _ in 0..self.start_segment_index {
-            comps.next();
-        }
-        comps.as_path()
-    }
-
-    /// Get the absolute path.
-    pub fn get_absolute_path(&self) -> &Path {
-        self.absolute_path.as_path()
-    }
-
-    /// Crops the path from the `start_segment_index` to the given `end_segment_index` (exclusive).
-    pub fn trim(&mut self, end_segment_index: usize) {
-        self.absolute_path = self
-            .absolute_path
-            .iter()
-            .take(self.start_segment_index + end_segment_index)
-            .collect();
-    }
-
-    /// Pushes a directory to the current location.
-    pub fn push<P: AsRef<Path>>(&mut self, directory_name: P) {
-        self.absolute_path.push(directory_name);
     }
 }
 
@@ -98,6 +117,7 @@ pub struct DirectoryLastModifiedTime(pub SystemTime);
 #[derive(Component)]
 pub struct AssetBrowserNode;
 
+// TODO: use bevy_panel_layout
 fn ui_setup(
     mut commands: Commands,
     root: Query<Entity, With<AssetBrowserNode>>,
@@ -122,34 +142,25 @@ fn ui_setup(
         });
 }
 
-/// A system to automatically refresh the current directory content when the directory is modified.
-pub fn refresh_directory_content(
-    mut commands: Commands,
-    content_list_query: Query<(Entity, Option<&Children>), With<ScrollingList>>,
-    mut last_modified_time: ResMut<DirectoryLastModifiedTime>,
-    mut location: ResMut<AssetBrowserLocation>,
-    theme: Res<Theme>,
-    asset_server: Res<AssetServer>,
-) {
-    directory_content::refresh_content(
-        &mut commands,
-        &content_list_query,
-        &mut last_modified_time,
-        &mut location,
-        &theme,
-        &asset_server,
-    );
-}
-
 /// Every type of button in the asset browser
 #[derive(Component, Clone, Copy, PartialEq, Eq, Debug)]
 pub enum ButtonType {
     /// A Path segment of the current asset browser location
-    /// When clicked, the asset browser will navigate to the corresponding directory
-    LocationSegment,
+    LocationSegment(LocationSegmentType),
     /// An asset button
     /// Used to interact with the assets in the directory content view
     AssetButton(AssetType),
+}
+
+/// All the types of location segments
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+pub enum LocationSegmentType {
+    /// The root segment, is an extra segment that mean that your nowwhere and wish to see what sources are available
+    Root,
+    /// A source segment, is a segment that represent a source
+    Source,
+    /// A directory segment, is a segment that represent a directory relative to the source root
+    Directory,
 }
 
 /// Every type of asset the asset browser supports
@@ -161,23 +172,37 @@ pub enum AssetType {
     /// A directory asset
     /// When clicked, the asset browser will step into the directory
     Directory,
+    /// Special type of Asset that is used to represent the engine source
+    EngineSource,
 }
 
 /// Map the asset type to the corresponding icon
-pub fn content_button_to_icon<A: Asset>(
+pub fn content_button_to_icon(
     asset_type: &AssetType,
     asset_server: &Res<AssetServer>,
-) -> Handle<A> {
+) -> Handle<Image> {
     match asset_type {
         AssetType::Directory => {
-            asset_server.load::<A>("embedded://bevy_asset_browser/assets/directory_icon.png")
+            asset_server.load("embedded://bevy_asset_browser/assets/directory_icon.png")
         }
-        _ => asset_server.load::<A>("embedded://bevy_asset_browser/assets/file_icon.png"),
+        AssetType::EngineSource => {
+            asset_server.load("embedded://bevy_asset_browser/assets/source_icon.png")
+        }
+        _ => asset_server.load("embedded://bevy_asset_browser/assets/file_icon.png"),
+    }
+}
+
+const DEFAULT_SOURCE_ID_NAME: &str = "Default";
+
+/// Convert the asset source id to a string
+pub fn asset_source_id_to_string(asset_source_id: &AssetSourceId) -> String {
+    match asset_source_id {
+        AssetSourceId::Default => DEFAULT_SOURCE_ID_NAME.to_string(),
+        AssetSourceId::Name(name) => name.to_string(),
     }
 }
 
 /// Handle the asset browser button interactions
-#[allow(clippy::too_many_arguments)]
 pub fn button_interaction(
     mut commands: Commands,
     mut interaction_query: Query<
@@ -190,79 +215,86 @@ pub fn button_interaction(
         ),
         (Changed<Interaction>, With<Button>),
     >,
-    content_list_query: Query<(Entity, Option<&Children>), With<ScrollingList>>,
-    path_list_query: Query<(Entity, &Children), With<TopBarNode>>,
+    path_list_query: Query<&Children, With<TopBarNode>>,
     text_query: Query<&Text>,
-    theme: Res<Theme>,
     mut location: ResMut<AssetBrowserLocation>,
-    mut last_modified_time: ResMut<DirectoryLastModifiedTime>,
-    asset_server: Res<AssetServer>,
+    mut asset_sources_builder: ResMut<AssetSourceBuilders>,
+    one_shot_systems: Res<AssetBrowserOneShotSystems>,
 ) {
     for (button_entity, interaction, button_type, mut background_color, button_children) in
         &mut interaction_query
     {
         match *interaction {
             Interaction::Pressed => {
-                let location_has_changed = match button_type {
-                    ButtonType::LocationSegment => {
-                        let (path_list_entity, path_list_children) = path_list_query.single();
+                let location_as_changed = match button_type {
+                    ButtonType::LocationSegment(LocationSegmentType::Root) => {
+                        location.source_id = None;
+                        location.path.clear();
+                        true
+                    }
+                    ButtonType::LocationSegment(LocationSegmentType::Source) => {
+                        location.path.clear();
+                        true
+                    }
+                    ButtonType::LocationSegment(LocationSegmentType::Directory) => {
+                        let path_list_children = path_list_query.single();
                         // Last segment is the current directory, no need to reload
                         if button_entity == *path_list_children.last().unwrap() {
                             return;
                         }
                         let segment_position = path_list_children
                             .iter()
-                            .skip(1) // First child is a separator
                             .step_by(2) // Step by 2 to go through each segment, skipping the separators
+                            .skip(1) // Skip the "Sources" segment
                             .position(|child| *child == button_entity)
                             .unwrap();
-                        location.trim(segment_position + 1);
-                        let child_to_remove = &path_list_children[(segment_position + 1) * 2..];
-                        for child in child_to_remove {
-                            commands.entity(*child).despawn_recursive();
+                        location.path = location.path.iter().take(segment_position).collect();
+                        true
+                    }
+                    ButtonType::AssetButton(AssetType::EngineSource) => {
+                        let source_name =
+                            &text_query.get(button_children[1]).unwrap().sections[0].value;
+                        if source_name == DEFAULT_SOURCE_ID_NAME {
+                            location.source_id = Some(AssetSourceId::Default);
+                        } else {
+                            location.source_id = asset_sources_builder
+                                .build_sources(false, false)
+                                .iter()
+                                .find(|source| match source.id() {
+                                    AssetSourceId::Name(CowArc::Static(name)) => {
+                                        name == source_name
+                                    }
+                                    _ => false,
+                                })
+                                .map(AssetSource::id);
                         }
-                        commands
-                            .entity(path_list_entity)
-                            .remove_children(child_to_remove);
                         true
                     }
                     ButtonType::AssetButton(AssetType::Directory) => {
                         let directory_name =
                             &text_query.get(button_children[1]).unwrap().sections[0].value;
-                        location.push(directory_name);
-                        let (path_list_entity, _) = path_list_query.single();
-                        commands.entity(path_list_entity).with_children(|parent| {
-                            top_bar::push_path_segment(
-                                parent,
-                                directory_name.clone(),
-                                theme.as_ref(),
-                            );
-                        });
+                        location.path.push(directory_name);
                         true
                     }
                     _ => false,
                 };
 
-                if location_has_changed {
-                    last_modified_time.0 = SystemTime::UNIX_EPOCH; // Force refresh
-                    directory_content::refresh_content(
-                        &mut commands,
-                        &content_list_query,
-                        &mut last_modified_time,
-                        &mut location,
-                        &theme,
-                        &asset_server,
+                if location_as_changed {
+                    commands.run_system(
+                        one_shot_systems.0[OneShotSystem::FetchDirectoryContent as usize],
                     );
+                    commands
+                        .run_system(one_shot_systems.0[OneShotSystem::RefreshTopBarUi as usize]);
                 }
             }
             Interaction::Hovered => match button_type {
-                ButtonType::LocationSegment | ButtonType::AssetButton(AssetType::Directory) => {
+                ButtonType::LocationSegment(_) | ButtonType::AssetButton(AssetType::Directory) => {
                     background_color.0 = Color::srgb(0.5, 0.5, 0.5); // TODO: Use theme
                 }
                 _ => {}
             },
             Interaction::None => match button_type {
-                ButtonType::LocationSegment => {
+                ButtonType::LocationSegment(_) => {
                     background_color.0 = top_bar::PATH_SEGMENT_BACKGROUND_COLOR;
                 }
                 ButtonType::AssetButton(AssetType::Directory) => {

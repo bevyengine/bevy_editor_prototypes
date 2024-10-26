50e8bf8642c9f0e9047f5d23a601963fc7ca34df
Joona Aalto <jondolf.dev@gmail.com>
2024-10-26 03:41:25 +0300

    Change Z axis color

--- crates/bevy_infinite_grid/src/lib.rs
+++ crates/bevy_infinite_grid/src/lib.rs
@@ -43,7 +43,7 @@ impl Default for InfiniteGridSettings {
     fn default() -> Self {
         Self {
             x_axis_color: Color::srgb(1.0, 0.2, 0.2),
-            z_axis_color: Color::srgb(0.2, 0.2, 1.0),
+            z_axis_color: Color::srgb(0.175, 0.55, 1.0),
             minor_line_color: Color::srgb(0.1, 0.1, 0.1),
             major_line_color: Color::srgb(0.25, 0.25, 0.25),
             fadeout_distance: 100.,

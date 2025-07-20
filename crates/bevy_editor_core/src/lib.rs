4f83bfc817c18cfb5da02977dfbe428768349039
Tim <JustTheCoolDude@gmail.com>
2025-07-20 20:16:29 +0200

    Humble Beginnings for the `ActionRegistry` (#234)

--- crates/bevy_editor_core/src/lib.rs
+++ crates/bevy_editor_core/src/lib.rs
@@ -1,11 +1,17 @@
 //! This crate provides core functionality for the Bevy Engine Editor.
 
+pub mod actions;
 pub mod selection;
 pub mod utils;
 
 use bevy::prelude::*;
 
-use crate::{selection::SelectionPlugin, utils::CoreUtilsPlugin};
+use crate::{actions::ActionsPlugin, selection::SelectionPlugin, utils::CoreUtilsPlugin};
+
+/// Crate prelude.
+pub mod prelude {
+    pub use crate::actions::{ActionAppExt, ActionWorldExt};
+}
 
 /// Core plugin for the editor.
 #[derive(Default)]
@@ -13,6 +19,6 @@ pub struct EditorCorePlugin;
 
 impl Plugin for EditorCorePlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins((SelectionPlugin, CoreUtilsPlugin));
+        app.add_plugins((ActionsPlugin, SelectionPlugin, CoreUtilsPlugin));
     }
 }

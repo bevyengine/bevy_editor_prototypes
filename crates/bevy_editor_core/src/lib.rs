b81adf1c9ddc5555a485a2f1b495e0d37612f190
Tim <JustTheCoolDude@gmail.com>
2025-08-04 18:09:14 +0200

    Rework action keybinding (#243)
    * Rework keybindings
    
    * Reorder keybinding constructor parameters

--- crates/bevy_editor_core/src/lib.rs
+++ crates/bevy_editor_core/src/lib.rs
@@ -1,17 +1,22 @@
 //! This crate provides core functionality for the Bevy Engine Editor.
 
 pub mod actions;
+pub mod keybinding;
 pub mod selection;
 pub mod utils;
 
 use bevy::prelude::*;
 
-use crate::{actions::ActionsPlugin, selection::SelectionPlugin, utils::CoreUtilsPlugin};
+use crate::{
+    actions::ActionsPlugin, keybinding::KeybindingPlugin, selection::SelectionPlugin,
+    utils::CoreUtilsPlugin,
+};
 
 /// Crate prelude.
 pub mod prelude {
     pub use crate::{
         actions::{ActionAppExt, ActionWorldExt},
+        keybinding::{Keybinding, KeybindingAppExt},
         selection::EditorSelection,
         utils::IntoBoxedScene,
     };
@@ -23,6 +28,11 @@ pub struct EditorCorePlugin;
 
 impl Plugin for EditorCorePlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins((ActionsPlugin, SelectionPlugin, CoreUtilsPlugin));
+        app.add_plugins((
+            ActionsPlugin,
+            KeybindingPlugin,
+            SelectionPlugin,
+            CoreUtilsPlugin,
+        ));
     }
 }
